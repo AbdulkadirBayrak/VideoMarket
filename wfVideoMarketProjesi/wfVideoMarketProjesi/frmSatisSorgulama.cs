171f55f1ff5916442875eb59564b285e32e640c9
AbdulkadirBayrak <bayrak.akadir@gmail.com>
2016-11-28 18:40:04 +0200

    VideoMarket Projesi

---
+++ wfVideoMarketProjesi/wfVideoMarketProjesi/frmSatisSorgulama.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace wfVideoMarketProjesi
+{
+    public partial class frmSatisSorgulama : Form
+    {
+        public frmSatisSorgulama()
+        {
+            InitializeComponent();
+        }
+
+        private void btngetir_Click(object sender, EventArgs e)
+        {
+            cFilmSatis fs = new cFilmSatis();
+            DataTable dt = fs.SatislariGetirByTarihlerArasi(dtpTarih1.Value, dtpTarih2.Value);
+            dgvsatislar.DataSource = dt;
+        }
+    }
+}
